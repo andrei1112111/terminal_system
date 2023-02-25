c62558056d66328a42049e446f4c7bc70dd80850
andrei1112111 <atishkin060705@gmail.com>
2023-02-26 00:10:20 +0700

    without functions

--- main2.py
+++ main2.py
@@ -1,19 +1,32 @@
 import pygame
 from pygame.locals import *
+import requests
+import time
+import json
 
-START_TEXT = [
-    'Welcome to the new Level 1 terminal software'
-    'created by Tishkin Andrei ',
-    '          |          ',
-    '          |          ',
-    '          |          ',
-    '          |          ',
-]
-
+json_data = json.load(open('data.json'))
 DISPLAY_SIZE = (1920, 1080)
 DEFAULT_BACKGROUND_COLOUR = (4, 3, 0)
 SYMBOLS_OFFSET = (20, 15)
+SCROLL_SPEED = 1.5
 FPS = 85
+# 84 символа в строке
+DEFAULT_LOCK = [0, 15]
+START_TEXT = [
+    f'      Welcome to the new Level {json_data["software"]} terminal software created by Tishkin Andrei',
+    '',
+    'The Python version is Python 3.11.0',
+    'Pygame is used to render the terminal',
+    'And also: Requests, Time, Json',
+    f'Last update: {json_data["last_update"]}',
+    '',
+    '',
+    '',
+    '               Github repository: https://github.com/andrei1112111/terminal_system',
+    '',
+    '',
+    f'{33 * " "}---PRESS A KEY---'
+]
 
 trans_kirill = ["йцукенгшщзхъфывапролджэёячсмитьбю", "qwertyuiop[]asdfghjkl;'\zxcvbnm,."]
 
@@ -36,7 +49,7 @@ class Console:
         self.data = default_data
         self.FONT = pygame.font.Font(self.DEFAULT_NORMAL_FONT, self.DEFAULT_FONT_SIZE)
         self.SYMBOL_SIZE = (self.DEFAULT_FONT_SIZE, self.DEFAULT_FONT_SIZE)
-        self.lock = [5, 0]
+        self.lock = DEFAULT_LOCK
 
     def render(self, offset):
         offset = SYMBOLS_OFFSET[1] - offset
@@ -47,6 +60,13 @@ class Console:
                     (SYMBOLS_OFFSET[0], offset)
                 )
             offset += self.SYMBOL_SIZE[1]
+        if offset >= DISPLAY_SIZE[1] - self.DEFAULT_FONT_SIZE * 2 - SYMBOLS_OFFSET[1]:
+            return True
+
+    def append(self, string, blocked: bool = False):
+        self.data.append(string)
+        if blocked:
+            self.lock = [len(string), len(self.data)]
 
     def add(self, symbol):
         if self.data == ['']:
@@ -59,6 +79,10 @@ class Console:
             self.data.append(symbol)
 
     def remove(self, b: bool = True, perm: bool = False):
+        if len(self.data[-1]) == 0 and len(self.data) <= self.lock[1]:
+            return
+        elif len(self.data) == self.lock[1] and len(self.data[-1]) <= self.lock[0]:
+            return
         if perm:
             self.data[-1] = self.data[-1][:-1]
             return
@@ -74,13 +98,13 @@ class Console:
         if not self.data:
             self.data = ['']
 
-    def new_line(self):
-        print(self.data, '--')
+    def new_line(self, n):
+        if n == 1:
+            return self.data[-1][self.lock[0]:].replace('¦', '')
         if self.data == ['']:
             self.data.append(cursor_symbol[0])
         if self.data[-1] and self.data[-1][-1] == cursor_symbol[0]:
             console.remove(perm=True)
-
         self.data.append('')
 
     def get(self, el: int | str = -1):
@@ -96,6 +120,8 @@ class Console:
 console = Console(DISPLAY_SIZE, START_TEXT)
 cursor_symbol = "¦"
 screen_offset = 0
+current_system_pos = 'wait_key'
+lines_for_answer = 1
 
 ONE_PER_SECOND = pygame.USEREVENT + 1
 pygame.time.set_timer(ONE_PER_SECOND, 1000)
@@ -104,32 +130,46 @@ pygame.time.set_timer(TEN_PER_SECOND, 80)
 
 while True:
     for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                exit()
-            elif event.key == K_RETURN:
-                console.new_line()
-        if event.type == TEN_PER_SECOND:
-            keys = pygame.key.get_pressed()
-            if keys[K_BACKSPACE]:
-                if console.get() == cursor_symbol[0]:
+        if current_system_pos == 'typing':
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    exit()
+                elif event.key == K_RETURN:
+                    answer = console.new_line(lines_for_answer)
+                    print(answer)
+            if event.type == TEN_PER_SECOND:
+                keys = pygame.key.get_pressed()
+                if keys[K_BACKSPACE]:
+                    if console.get() == cursor_symbol[0]:
+                        console.remove()
                     console.remove()
-                console.remove()
-        if event.type == TEXTINPUT:
-            if console.get() == cursor_symbol[0]:
-                console.remove(perm=True)
-            if event.text[0] in trans_kirill[0]:
-                event.text = ''.join([trans_kirill[1][trans_kirill[0].index(i)] for i in event.text])
-            console.add(event.text)
-        if event.type == ONE_PER_SECOND:
-            if console.get() == cursor_symbol[0]:
-                console.remove(False)
-            else:
-                console.add(cursor_symbol[0])
-
-    # print(console.get('all'))
+            if event.type == TEXTINPUT:
+                if console.get() == cursor_symbol[0]:
+                    console.remove(perm=True)
+                if event.text[0] in trans_kirill[0]:
+                    event.text = ''.join([trans_kirill[1][trans_kirill[0].index(i)] for i in event.text])
+                console.add(event.text)
+            if event.type == ONE_PER_SECOND:
+                if console.get() == cursor_symbol[0]:
+                    console.remove(False)
+                else:
+                    console.add(cursor_symbol[0])
+        elif current_system_pos == 'wait_key':
+            if event.type == KEYDOWN:
+                system_start = True
+                current_system_pos = 'select_hot'
+        elif current_system_pos == 'select_hot':
+            counter = 1
+            [console.append('') for i in range(5)]
+            for hot_function in json_data["hot_functions"]:
+                console.append(f'| {counter} |      {hot_function[0]: <12}      {hot_function[1]}')
+                counter += 1
+            console.append('')
+            console.append('Select: ', blocked=True)
+            current_system_pos = 'typing'
 
     screen_surface.fill(DEFAULT_BACKGROUND_COLOUR)
-    console.render(screen_offset)
+    if console.render(screen_offset):
+        screen_offset += SCROLL_SPEED
     pygame.display.flip()
     clock.tick(FPS)
